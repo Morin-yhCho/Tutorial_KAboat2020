c692bc9a916344ba4096a2f715ffb422cc73516f
morin_YCho <yhcho88@kaist.ac.kr>
2020-04-23 05:59:23 +0000

    Update GPS.py

--- GPS/GPS.py
+++ GPS/GPS.py
@@ -5,28 +5,30 @@ import operator
 ser = serial.Serial(port = "/dev/ttyACM0", baudrate = 38400, timeout = 0.1)	
 
 def GPSparser(data):
+	gps_data = list()
 	idx_rmc = data.find('GNRMC')
 	if data[idx_rmc:idx_rmc+5] == "GNRMC":
 		data = data[idx_rmc:]	
+		print data
 		if checksum(data):
 			spliteddata = data.split(",")
 			if spliteddata[2] == 'V':
 				print "data invalid"		
 	
 			elif spliteddata[2] == 'A':
-				gps_data[1] = float(spliteddata[1])
+				gps_data.append(float(spliteddata[1]))
 				if spliteddata[4] == 'N' :
-					gps_data[2] = float(spliteddata[3])
+					gps_data.append(float(spliteddata[3]))
 				else:
-					gps_data[2] = -1.0*float(spliteddata[3])
+					gps_data.append( -1.0*float(spliteddata[3]))
 	
 				if spliteddata[6] == 'E' :
-					gps_data[3] = float(sdata[5])
+					gps_data.append(float(spliteddata[5]))
 				else:
-					gps_data[3] = -1.0*float(sdata[5])
+					gps_data.append(-1.0*float(spliteddata[5]))
 					
-				gps_data[4] = float(sdata[7])
-				gps_data[5] = float(sdata[8])
+				gps_data.append(float(spliteddata[7]))
+				gps_data.append(float(spliteddata[8]))
 		
 				return gps_data 
 		else :
@@ -36,6 +38,7 @@ def checksum(sentence):
 	sentence = sentence.strip('\n')
 	nmeadata, cksum = sentence.split('*',1)
 	calc_cksum = reduce(operator.xor, (ord(s) for s in nmeadata), 0)
+	print int(cksum,16), calc_cksum
 	if int(cksum,16) == calc_cksum:
 		return True 
 	else:
@@ -44,5 +47,8 @@ def checksum(sentence):
 while 1: 
 	data = ser.readline()
 	gps_data  = GPSparser(data) 
-	print gps_data
+	print gps_data 
+
+
+
 	
